779c72d8fa08034fc05632021d86f9835d42d253
Gollapudi, Meera <Gollapudi.Meera@principal.com>
2021-03-01 13:40:49 -0600

    No change in the code since i had updates in the method - which is called here.

--- src/test/java/WeekOneExercisesPartTwo.java
+++ src/test/java/WeekOneExercisesPartTwo.java
@@ -10,7 +10,7 @@ public class WeekOneExercisesPartTwo {
     // TODO Response: Why would you never refactor if you had a failing test?
 
     @Test
-    //@Disabled
+    @Disabled
     public void bloodAlcoholLevelForAMale() {
         char gender = 'M';
         int weightInPounds = 185;
@@ -24,7 +24,7 @@ public class WeekOneExercisesPartTwo {
     }
 
     @Test
-    @Disabled
+    //@Disabled
     public void bloodAlcoholLevelForAFemale() {
         char gender = 'F';
         int weightInPounds = 135;
