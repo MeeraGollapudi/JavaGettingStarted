2aacd0ce96c7d14d6125b9f9238108b618af9fd2
Gollapudi, Meera <Gollapudi.Meera@principal.com>
2021-03-01 13:35:15 -0600

    Updated method calculateBAC to do the calculation to get the BAC. declared all the fixed values as a variable and called these variables in the calculation. calculated the weight quotient by dividing 5.14 and the weight in  pounds. then depending on the gender - if statement to get the peak BAC. then taking the peak BAC and substracting it from product of hours since lastdrink with 0.015 - i got the actual BAC.

--- src/test/java/WeekOneExercisesPartTwo.java
+++ src/test/java/WeekOneExercisesPartTwo.java
@@ -10,12 +10,13 @@ public class WeekOneExercisesPartTwo {
     // TODO Response: Why would you never refactor if you had a failing test?
 
     @Test
-    @Disabled
+    //@Disabled
     public void bloodAlcoholLevelForAMale() {
         char gender = 'M';
         int weightInPounds = 185;
         int ouncesOfAlcoholConsumed = 3;
         float hoursSinceLastDrink = 2;
+
 
         float actualBAC = calculateBAC(gender, weightInPounds, ouncesOfAlcoholConsumed, hoursSinceLastDrink);
 
@@ -30,6 +31,7 @@ public class WeekOneExercisesPartTwo {
         int ouncesOfAlcoholConsumed = 5;
         float hoursSinceLastDrink = 1;
 
+        // float actualBAC = calculateBAC(gender, weightInPounds, ouncesOfAlcoholConsumed, hoursSinceLastDrink);
         float actualBAC = calculateBAC(gender, weightInPounds, ouncesOfAlcoholConsumed, hoursSinceLastDrink);
 
         assertEquals(0.11, actualBAC, .001);
@@ -39,13 +41,13 @@ public class WeekOneExercisesPartTwo {
      * Given your weight, gender, number of drinks, the amount of alcohol
      * by volume of the drinks consumed, and the amount of time since your last drink,
      * calculate your Blood Alcohol Content (BAC).
-     *
+     * <p>
      * The weight quotient is 5.14 divided by the person's weight in pounds.
      * The alcohol distribution ratio is 0.73 for men and 0.66 for women.
      * The peak alcohol content is the product of the weight quotient, ounces of alcohol consumed, and the
      * alcohol distribution ratio.
      * The last consumption interval is the product of the hours since the last drink and 0.015.
-     *
+     * <p>
      * The Blood Alcohol Content is the difference between the peak alcohol content and the last consumption interval.
      *
      * @param gender
@@ -57,7 +59,20 @@ public class WeekOneExercisesPartTwo {
 
     private float calculateBAC(char gender, int weightInPounds, int ouncesOfAlcoholConsumed, float hoursSinceLastDrink) {
         // TODO RESPONSE: In the PluralSight video, you learned of three ways to make decisions based on the value
-        //   in the gender variable. Implement this method using one way, and describe how the other ways could work.
-        return 0.0f;
+        //        //   in the gender variable. Implement this method using one way, and describe how the other ways could work.
+        float actualBAC;
+        float weightQuotient;
+        float peakBAC;
+        float weightConstant = 5.14f;
+        float menAlcoholDist = 0.73f;
+        float womenAlcoholDist = 0.66f;
+        weightQuotient = weightConstant / weightInPounds;
+        if (gender == 'M') {
+            peakBAC = (weightQuotient * ouncesOfAlcoholConsumed * menAlcoholDist);
+        } else {
+            peakBAC = (weightQuotient * ouncesOfAlcoholConsumed * womenAlcoholDist);
+        }
+        actualBAC = peakBAC - (hoursSinceLastDrink * 0.015f);
+        return actualBAC;
     }
 }
